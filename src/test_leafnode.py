ca88f2beb0782f89ae80a0b46cda31fcfd99a345
Joshua Tuddenham <joshuaisaact@gmail.com>
2025-11-19 06:23:15 +0000

    feat: leaf nodes implemented

---
+++ src/test_leafnode.py
@@ -0,0 +1,28 @@
+import unittest
+
+from leafnode import LeafNode
+
+
+class TestLeafNode(unittest.TestCase):
+    def test_to_html_no_tag(self):
+        node = LeafNode(None, "Just text")
+        self.assertEqual(node.to_html(), "Just text")
+
+    def test_to_html_simple(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_to_html_with_props(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(
+            node.to_html(), '<a href="https://www.google.com">Click me!</a>'
+        )
+
+    def test_to_html_no_value(self):
+        node = LeafNode("p", None)
+        with self.assertRaises(ValueError):
+            node.to_html()
+
+
+if __name__ == "__main__":
+    unittest.main()
